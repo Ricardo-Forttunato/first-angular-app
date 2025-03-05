0fa4a034980546c576fc93b4455d504cacd6f1fb
Ricardo-Forttunato <ricardo.forttunato@gmail.com>
2025-03-05 12:30:34 -0300

    add novo componente new-task, implementando recurso two-way binding

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
- 
   @Input({required: true}) userId!: string; 
   //a alternative way to use question mark(?) is to use union type like this: @Input() name: string | undefined;
   @Input({required: true}) name?: string;
+
+  isAddingTask = false;
 
   tasks = [
     {
@@ -41,5 +43,17 @@ export class TasksComponent {
 
   get selectedUserTasks () {
     return this.tasks.filter(task => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string){
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
   }
 }
