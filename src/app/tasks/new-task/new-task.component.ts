2af77fdb80d45e9cce4fee5e3175a4e10f6bc9e3
Ricardo-Forttunato <ricardo.forttunato@gmail.com>
2025-03-06 16:39:41 -0300

    add melhorias para a interface e novos arquivos

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,21 +1,43 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, signal, inject, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
-  selector: 'app-new-task',
-  imports: [FormsModule],
-  templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+	selector: 'app-new-task',
+	imports: [FormsModule],
+	templateUrl: './new-task.component.html',
+	styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output () cancel = new EventEmitter<void>();
+	@Input({ required: true }) userId!: string;
+	@Output() close = new EventEmitter<void>();
+	// @Output () add = new EventEmitter<NewTaskData>();
 
-  enteredTitle = '';
-  enteredSummary = '';
-  enteredDate = '';
+	// two-way binding without signals
+	enteredTitle = '';
+	enteredSummary = '';
+	enteredDate = '';
+	// two-way binding with signals
+	// enteredTitle = signal('');
+	// enteredSummary = signal('');
+	// enteredDate = signal('');
 
+	private tasksService: TasksService = inject(TasksService);
 
-  onCancel () {
-    this.cancel.emit();
-  }
+	onCancel() {
+		this.close.emit();
+	}
+
+	onSubmit() {
+		this.tasksService.addtasks(
+			{
+				title: this.enteredTitle,
+				summary: this.enteredSummary,
+				date: this.enteredDate
+			},
+			this.userId
+		);
+		this.close.emit();
+	}
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,9 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, Inject } from '@angular/core';
+
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -12,48 +15,21 @@ export class TasksComponent {
   @Input({required: true}) userId!: string; 
   //a alternative way to use question mark(?) is to use union type like this: @Input() name: string | undefined;
   @Input({required: true}) name?: string;
-
   isAddingTask = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ]
-
-  get selectedUserTasks () {
-    return this.tasks.filter(task => task.userId === this.userId);
+  constructor (@Inject(TasksService) private tasksService: TasksService) {
+    this.tasksService = tasksService;
   }
 
-  onCompleteTask(id: string){
-    this.tasks = this.tasks.filter(task => task.id !== id);
+  get selectedUserTasks () {
+    return this.tasksService.getUsertasks(this.userId);
   }
 
   onStartAddTask(){
     this.isAddingTask = true;
   }
 
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingTask = false;
   }
 }
